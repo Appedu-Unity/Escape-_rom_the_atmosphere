2c34852aa5f97cd9f71002b2bdfdf06d78840c06
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2021-08-22 02:00:54 +0800

    更新

---
+++ Escape from the atmosphere/Assets/Script/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void StartGame()
+    {
+        Invoke("DelayStart", 1.3f);
+    }
+    private void DelayStart()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        Invoke("DelayQuit", 1.5f);
+    }
+    private void DelayQuit()
+    {
+        Application.Quit();
+    }
+}
